7e1e4a1d1b5d718f0b9c4f7b6a2bc78721c5630d
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-09 21:25:27 -0300

    Add files via upload

---
+++ ggplot2_datacamp.R
@@ -0,0 +1,22 @@
+
+# Pacote ggplot2 ---------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 09/11/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://www.datacamp.com/cheat-sheet/ggplot2-cheat-sheet?utm_source=twitter&utm_medium=organic_social&utm_campaign=22 --------
+
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### ggplot2 é considerado um dos mais robustos pacotes de visualização de dados
+### em qualquer linguagem de programação. 
+
+### Ter capacidade na visualização de dados são apostas para qualquer pessoa que 
+### procure aumentar as suas capacidades no R. ggplot2 é um dos principais pacotes do R,
+### pois permite uma acessível abordagem para construir robustas visualizações no R.
+### No cheat sheet anexado a este github você terá um útil guia para todas as funções
+### e técnicas para iniciar o ggplot2 no R.
+
+# A Gramática de Gráficos ------------------------------------------------------------------------------------------------------------------
+
+
+
+
